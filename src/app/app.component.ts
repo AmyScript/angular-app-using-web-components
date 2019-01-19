caf6e0b92321a95d09df1c62e7767c0bd32a9fb9
Amy Wah <amy.wah@rangle.io>
2019-01-18 20:22:35 -0500

    in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'web-components';
+  city = '';
+  hourlyConditions = [];
+  currentConditions = null;
+  currentDate = '';
+  queryLocation = '';
+  showHourly = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSelect() {
+    console.log('button clicked');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import '@amyscript/dsg-info-card';
+import '@amyscript/dsg-input';
+import '@amyscript/dsg-input-button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
